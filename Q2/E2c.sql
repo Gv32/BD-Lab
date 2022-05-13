43b4db02da481e22f7a67d8abcac7ff8a161eb11
Gv32 <s284383@studenti.polito.it>
2022-05-13 09:09:50 +0200

    finito

--- Q2/E2c.sql
+++ Q2/E2c.sql
@@ -1,12 +1,14 @@
-select NomeT, Cognome, COUNT(Distinct CodS)
-from Istruttore I, Palestra P, Lezione L
-where I.CodFiscale = L.CodFiscale
-    and P.CodP = L.CodP
-    and P.Città = I.Città
-group by NomeT, Cognome
-having COUNT(CodP) = (
-    select COUNT(P1.CodP)
-    from Palestra P1
-    where P1.Città = P.Città
-    group by P1.CodP
-)
+select I0.NomeT, I0.Cognome, Spec
+from Istruttore I0, (   select P1.Citta NomiCitta, count(P1.CodP) as NP
+                        from Palestra P1
+                        group by P1.Citta),(    select I.Citta as Cit, I.CodFiscale as CodF, I.NomeT, I.Cognome, count(distinct P.CodP) as PA, count(distinct S.CodS) as Spec
+                                                from Istruttore I, Palestra P, Lezione L, Specialita S
+                                                where I.CodFiscale = L.CodFiscale
+                                                    and S.codS = L.CodS
+                                                    and P.CodP = L.CodP
+                                                    and P.Citta = I.Citta
+                                                group by I.Citta, I.CodFiscale, I.NomeT, I.Cognome)
+where NomiCitta = Cit
+    and CodF = I0.CodFiscale
+    and PA = NP
+group by I0.NomeT, I0.Cognome, Spec
